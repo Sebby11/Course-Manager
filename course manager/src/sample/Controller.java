97cf253c143da10eba4b45051442a20e939f050b
Drjenky <37814896+Drjenky@users.noreply.github.com>
2019-04-13 16:33:22 -0700

    Code for a super basic layout

--- course manager/src/sample/Controller.java
+++ course manager/src/sample/Controller.java
@@ -1,4 +1,75 @@
 package sample;
 
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+import javax.xml.soap.Text;
+
 public class Controller {
+    TableView table;
+
+    Controller(TableView table) {
+        this.table = table;
+    }
+
+    public static void addTask() {
+
+        Label title = new Label("Add task");
+        title.setFont(Font.font("Courier", 30));
+
+        //ENTER WHAT
+        Label whatLabel = new Label("What will you be doing?");
+
+        TextField what = new TextField();
+
+        //ENTER DESCRIPTION
+        Label descripLabel = new Label("Add description");
+
+        TextField descript = new TextField();
+
+        //Enter DUE
+        Label dueLabel = new Label("When will it be due?");
+
+        TextField due = new TextField();
+
+        //FINISH
+        Button finish = new Button("Done");
+
+        VBox layout = new VBox();
+        layout.setSpacing(1);                  //Spacing between components
+
+        //Set margins for components
+        layout.setMargin(whatLabel, new Insets(10, 20, 10, 20));
+        layout.setMargin(what, new Insets(10, 20, 10, 20));
+        layout.setMargin(descripLabel, new Insets(10, 20, 10, 20));
+        layout.setMargin(descript, new Insets(10, 20, 10, 20));
+        layout.setMargin(dueLabel, new Insets(10, 20, 10, 20));
+        layout.setMargin(due, new Insets(10, 20, 10, 20));
+        layout.setMargin(finish, new Insets(10, 20, 10, 20));
+
+        //Create a list & add components
+        ObservableList list = layout.getChildren();
+        list.addAll(title, whatLabel, what, descripLabel, descript, dueLabel, due, finish);
+
+        Scene addTaskScene = new Scene(layout, 300, 440);
+
+        //New window creation
+        Stage newWindow = new Stage();
+        newWindow.setTitle("Add Task");
+        newWindow.setScene(addTaskScene);
+        newWindow.setResizable(false);
+
+        newWindow.show();
+    }
 }
--- course manager/src/sample/Main.java
+++ course manager/src/sample/Main.java
@@ -1,21 +1,28 @@
 package sample;
 
-import com.sun.tools.hat.internal.model.Root;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
+import javafx.scene.Cursor;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+
+    double orgSceneX, orgSceneY;
+    double orgTranslateX, orgTranslateY;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -31,13 +38,27 @@ public class Main extends Application {
         TableView table = new TableView();
         table.setEditable(true);
         table.setLayoutY(100);
-        table.setLayoutX(310);
+        table.setLayoutX(200);
 
-        TableColumn whatCol = new TableColumn("What");
-        TableColumn descriptCol = new TableColumn("Description");
-        TableColumn dueCol = new TableColumn("Due");
+        TableColumn year = new TableColumn("Year");
+        year.setMinWidth(100);
+        TableColumn fall = new TableColumn("Fall");
+        fall.setMinWidth(150);
+        TableColumn winter = new TableColumn("Winter");
+        winter.setMinWidth(150);
+        TableColumn spring = new TableColumn("Spring");
+        spring.setMinWidth(150);
+        TableColumn summer = new TableColumn("(Summer)");
+        summer.setMinWidth(150);
+        table.getColumns().addAll(year, fall,winter,spring, summer);
 
-        table.getColumns().addAll(whatCol, descriptCol, dueCol);
+
+        Row r1 = new Row("row");
+
+        table.getItems().add(r1);
+        table.getItems().add(new Row("2nd"));
+        table.getItems().add(new Row("3rd"));
+        table.getItems().add(new Row("4th"));
 
 
         //Menu bar
@@ -52,7 +73,7 @@ public class Main extends Application {
 
         //GE COURSES MENU
         Menu GE_Menu = new Menu("GE's");
-        menu.getItems().add(GE_Menu);
+        //menu.getItems().add(GE_Menu);
 
         MenuItem GE1 = new MenuItem("GE 1");
         GE_Menu.getItems().add(GE1);
@@ -64,6 +85,26 @@ public class Main extends Application {
 
         mb.setLayoutX(0);
         mb.setLayoutY(100);
+
+        //MenuTask
+        GE1.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent e) {
+                System.out.println("Testing");
+               Circle circle_Red = new Circle(50.0f, Color.RED);
+                circle_Red.setCursor(Cursor.HAND);
+                circle_Red.setOnMousePressed(circleOnMousePressedEventHandler);
+                circle_Red.setOnMouseDragged(circleOnMouseDraggedEventHandler);
+
+                Button course = new Button("I am a Gen ED!");
+                course.setCursor(Cursor.HAND);
+                course.setOnMousePressed(labelOnMousePressedEventHandler);
+                course.setOnMouseDragged(labelOnMouseDraggedEventHandler);
+
+                g.getChildren().add(course);
+
+            }
+        });
 
         //Add Sammy the Programmer
         ImageView sammyPic = new ImageView("sammy.png");
@@ -78,7 +119,7 @@ public class Main extends Application {
         g.getChildren().add(table);
         g.getChildren().add(mb);
 
-        Scene scene = new Scene(g, 600, 500);
+        Scene scene = new Scene(g, 950, 650);
         scene.setFill(Color.rgb(255, 252, 105));
 
         primaryStage.setScene(scene);
@@ -87,8 +128,73 @@ public class Main extends Application {
 
     }
 
-
+    public class Row {
+        private String name;
+        public Row(String name) {
+            this.name = name;
+        }
+        public String getName() {
+            return name;
+        }
+    }
     public static void main(String[] args) {
         launch(args);
     }
+
+
+
+    EventHandler<MouseEvent> circleOnMousePressedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    orgSceneX = t.getSceneX();
+                    orgSceneY = t.getSceneY();
+                    orgTranslateX = ((Circle)(t.getSource())).getTranslateX();
+                    orgTranslateY = ((Circle)(t.getSource())).getTranslateY();
+                }
+            };
+
+    EventHandler<MouseEvent> circleOnMouseDraggedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    double offsetX = t.getSceneX() - orgSceneX;
+                    double offsetY = t.getSceneY() - orgSceneY;
+                    double newTranslateX = orgTranslateX + offsetX;
+                    double newTranslateY = orgTranslateY + offsetY;
+
+                    ((Circle)(t.getSource())).setTranslateX(newTranslateX);
+                    ((Circle)(t.getSource())).setTranslateY(newTranslateY);
+                }
+            };
+
+//Label movement
+    EventHandler<MouseEvent> labelOnMousePressedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    orgSceneX = t.getSceneX();
+                    orgSceneY = t.getSceneY();
+                    orgTranslateX = ((Button)(t.getSource())).getTranslateX();
+                    orgTranslateY = ((Button)(t.getSource())).getTranslateY();
+                }
+            };
+
+    EventHandler<MouseEvent> labelOnMouseDraggedEventHandler =
+            new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent t) {
+                    double offsetX = t.getSceneX() - orgSceneX;
+                    double offsetY = t.getSceneY() - orgSceneY;
+                    double newTranslateX = orgTranslateX + offsetX;
+                    double newTranslateY = orgTranslateY + offsetY;
+
+                    ((Button)(t.getSource())).setTranslateX(newTranslateX);
+                    ((Button)(t.getSource())).setTranslateY(newTranslateY);
+                }
+            };
 }
